c417585f355130912aa27d3cfbbfbffe957fbd91
Muyinda Rogers <moverr@gmail.com>
2020-02-04 17:06:29 +0300

    Debugging :

--- app/router.js
+++ app/router.js
@@ -8,5 +8,7 @@ export default class Router extends EmberRouter {
 
 Router.map(function() {
   this.route('about');
-  this.route('contactus')
+  this.route('contactus');
+  this.route('contactus',{path:'minor'});
+  
 });
